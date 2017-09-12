138682ab9852b20723c2a0e34a1f157a60a9588f
Juan Tibaldi <jtibaldi09@gmail.com>
2017-09-12 10:59:40 -0300

    build.12092017.1

---
+++ CameraScale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraScale : MonoBehaviour {
+	public int targetWidth = 960;
+	public float pixelsToUnits = 1;
+
+	// Update is called once per frame
+	void Update () {
+		int height = Mathf.RoundToInt (targetWidth / (float)Screen.width * Screen.height);
+		Camera.main.orthographicSize = height / pixelsToUnits / 2;
+	}
+}
